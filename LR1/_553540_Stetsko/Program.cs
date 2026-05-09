f4abf8063ad6f400e8d4c7749962d3ea08157202
yaroslave.s <yarikst2007@gmail.com>
2026-05-09 23:30:10 +0300

    add LR1

---
+++ LR1/_553540_Stetsko/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+       double number1;
+       double number2;
+
+       while (true)
+        {
+            Console.Write("Введите первое число : ");
+            string input = Console.ReadLine();
+
+            bool isNumber = double.TryParse(input, out number1);
+
+            if (isNumber)
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Неправильный ввод, попробуйте еще раз");
+            }
+        }
+
+        while (true)
+        {
+            Console.Write("Введите второе число : ");
+            string input = Console.ReadLine();
+
+            bool isNumber = double.TryParse(input, out number2);
+
+            if (isNumber && number2 != 0)
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Неправильный ввод, попробуйте еще раз");                
+            }
+        }
+
+        double result = number1 / number2;
+        Console.WriteLine("Результат : " + result);        
+    }
+}
